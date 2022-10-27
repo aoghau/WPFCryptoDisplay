74f22a88dddbc4bf08ed112939aabb30350ed44e
Kostyantin Bulgaka <kostyabulgaka3@gmail.com>
2022-10-27 19:22:24 +0300

    First feature logic implementation

--- DataAccess/CurrencyContext.cs
+++ DataAccess/CurrencyContext.cs
@@ -17,6 +17,6 @@ namespace DataAccess
             return currencies.data;
         }
 
-
+        public CurrencyContext() { }
     }
 }
--- ViewModel/BusinessLogic.cs
+++ ViewModel/BusinessLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace ViewModel
 {
-    internal class BusinessLogic
+    public class BusinessLogic
     {
+        CurrencyContext context;        
+
+        public async Task<List<Currency>> GetTopCurrencies()
+        {
+            List<Currency> currencies = await context.GetCurrencies();
+            List<Currency> topCurrencies = currencies.Where(u => Int32.Parse(u.rank) <= 10).ToList();
+            return topCurrencies;
+        }
+
+        public BusinessLogic()
+        {
+            context = new CurrencyContext();
+        }
+
     }
 }
