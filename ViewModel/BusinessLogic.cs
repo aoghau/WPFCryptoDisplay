d25bee833043d9ef5d8b070f5a93cf5e028c4838
Kostyantin Bulgaka <kostyabulgaka3@gmail.com>
2022-10-30 21:07:40 +0200

    Cleaning up

--- ViewModel/BusinessLogic.cs
+++ ViewModel/BusinessLogic.cs
@@ -1,6 +1,8 @@
 ﻿using DataAccess;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,20 +10,35 @@ using System.Threading.Tasks;
 namespace ViewModel
 {
     public class BusinessLogic
-    {
-        CurrencyContext context;        
+    {        
+        public ObservableCollection<Currency> Currencies { get; set; }
 
-        public async Task<List<Currency>> GetTopCurrencies()
+        public static async Task<List<Currency>> GetCurrenciesAsync()
         {
-            List<Currency> currencies = await context.GetCurrencies();
+            var client = new HttpClient();
+            var request = await client.GetStringAsync("http://api.coincap.io/v2/assets");
+            Wrapper currencies = JsonConvert.DeserializeObject<Wrapper>(request);
+            return currencies.data;
+        }
+
+        public async Task<ObservableCollection<Currency>> GetTopCurrencies()
+        {
+            List<Currency> currencies = await GetCurrenciesAsync();
             List<Currency> topCurrencies = currencies.Where(u => Int32.Parse(u.rank) <= 10).ToList();
-            return topCurrencies;
+            return new ObservableCollection<Currency>(topCurrencies);
         }
 
-        public BusinessLogic()
+        async public static Task<BusinessLogic> GetBusinessLogicAsync()
+        {           
+            ObservableCollection<Currency> currencies = new ObservableCollection<Currency>(await GetCurrenciesAsync());
+            return new BusinessLogic(currencies);
+        }
+
+        private BusinessLogic(ObservableCollection<Currency> data)
         {
-            context = new CurrencyContext();
+            this.Currencies = data;
         }
 
+        public BusinessLogic(){ }
     }
 }
--- WPFCryptoDisplay/MainWindow.xaml.cs
+++ WPFCryptoDisplay/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DataAccess;
+using ViewModel;
 
 namespace WPFCryptoDisplay
 {
@@ -23,6 +25,7 @@ namespace WPFCryptoDisplay
         public MainWindow()
         {
             InitializeComponent();
-        }
+            this.DataContext = new BusinessLogic();
+        }        
     }
 }
